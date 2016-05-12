018a1755ff18c99cf8aa6574f1e9367e48db4bd6
JangWooJin <ehdwlsdnwls@naver.com>
2016-05-13 00:49:09 +0900

    make init button action

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -12,21 +12,18 @@ public class Controller {
     private void clickLeftLoadButton(ActionEvent event){
         left_load_button.setText("Click");
     }
-
     @FXML
     private Button left_edit_button;
     @FXML
     private void clickLeftEditButton(){
         left_edit_button.setText("Click");
     }
-
     @FXML
     private Button left_save_button;
     @FXML
     private void clickLeftSaveButton(){
         left_save_button.setText("Click");
     }
-
     @FXML
     private Button right_load_button;
     @FXML
@@ -36,14 +33,23 @@ public class Controller {
     @FXML
     private Button right_edit_button;
     @FXML
-    private void clickRightEditButton(){
-        right_edit_button.setText("Click");
-    }
-
+    private void clickRightEditButton() { right_edit_button.setText("Click"); }
     @FXML
     private Button right_save_button;
     @FXML
     private void clickRightSaveButton(){
         right_save_button.setText("Click");
     }
+    @FXML
+    private Button compare_button;
+    @FXML
+    private void clickCompareButton() { compare_button.setText("Click"); }
+    @FXML
+    private Button copy_to_left_button;
+    @FXML
+    private void clickCopyToLeftButton() { copy_to_left_button.setText("Click"); }
+    @FXML
+    private Button copy_to_right_button;
+    @FXML
+    private void clickCopyToRightButton() { copy_to_right_button.setText("Click"); }
 }
