fb5cb7ca20bad4ff52df2b3c5b397f2e39b6e43f
Jooingong <bcw6622@naver.com>
2016-05-14 16:35:56 +0900

    "change model class file"

--- src/sample/Model.java
+++ src/sample/Model.java
@@ -1,7 +1,31 @@
 package sample;
-
+import java.lang.String;
+import java.util.ArrayList;
 /**
  * Created by User on 2016-05-14.
  */
-public class Model {
+abstract public class Model {
+
+    static abstract class SavedText
+    {
+        ArrayList<String> texts;
+
+        SavedText() {
+            texts = new ArrayList<String>();
+        }
+        abstract void Read(String s);
+        abstract void Write(String s);
+    }
+
+    SavedText t[];
+    ArrayList<String> groupsStart[];
+
+    Model()
+    {
+        t = new SavedText[2];
+        groupsStart = new ArrayList<String>[2];
+    }
+
 }
+
+
