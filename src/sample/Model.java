07fdbd17301507dcd14d2bdf005a7ac7b80c92e6
Jooingong <bcw6622@naver.com>
2016-05-14 21:25:39 +0900

    change texts data struct

--- src/sample/Model.java
+++ src/sample/Model.java
@@ -4,29 +4,46 @@ import java.util.ArrayList;
 /**
  * Created by User on 2016-05-14.
  */
+
 abstract public class Model {
 
-    static abstract class SavedText
+    abstract class SavedText
     {
         ArrayList<String> texts;
 
         SavedText() {
             texts = new ArrayList<String>();
         }
-        abstract void Read(String s);
-        abstract void Write(String s);
+        abstract void ReadFromOuter(String s);
+        abstract void WriteFromOuter(String s);
+
+        public String Read(int i) throws IndexOutOfBoundsException
+        {
+            return new String(texts.get(i));
+        }
+        public void Write(int i, String s) throws IndexOutOfBoundsException
+        {
+            texts.set(i,new String(s));
+        }
     }
 
-    SavedText t[];
+    final int NUMOFTEXTS = 2;
+    SavedText codes[];
     ArrayList[] groupsStart;
 
     Model()
     {
-        t = new SavedText[2];
-        groupsStart = new ArrayList[2];
-        groupsStart[0] = new ArrayList<String>();
-        groupsStart[1] = new ArrayList<String>();
+        codes = new SavedText[NUMOFTEXTS];
+        groupsStart = new ArrayList[NUMOFTEXTS];
+        for(int i=0;i<NUMOFTEXTS;i++) groupsStart[i] = new ArrayList<String>();
     }
+
+    public void textLinking(int i, SavedText s) throws IndexOutOfBoundsException
+    {
+        if(i<0 || i>=NUMOFTEXTS) throw new IndexOutOfBoundsException();
+        codes[i]=s;
+    }
+
 
 }
 
