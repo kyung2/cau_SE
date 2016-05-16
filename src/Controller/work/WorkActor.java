02449190b9008e9b98f28068a0972fb1d763d3fd
Jooingong <bcw6622@naver.com>
2016-05-16 13:00:52 +0900

    Worker now can read string

--- src/Controller/work/WorkActor.java
+++ src/Controller/work/WorkActor.java
@@ -10,18 +10,41 @@ public class WorkActor {
 
     private work w;
     WorkHave res;
-    private ArrayList<WorkActor> nextWorkActor;
+    private ArrayList<WorkActor> nextWorkActors;
     public WorkActor(work w)
     {
         this.w=w;
         res=null;
-        nextWorkActor = new ArrayList<WorkActor>();
+        nextWorkActors = new ArrayList<WorkActor>();
     }
     public WorkActor(work w, Object o[])
     {
         this.w=w;
         this.res=new WorkHave(o);
-        nextWorkActor = new ArrayList<WorkActor>();
+        nextWorkActors = new ArrayList<WorkActor>();
+    }
+    public WorkActor(String s) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+
+        this.w = ChangeStringToWork(s);
+        this.res=null;
+        nextWorkActors = new ArrayList<WorkActor>();
+    }
+    public WorkActor(String s, Object o[]) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+
+        this.w = ChangeStringToWork(s);
+        this.res=new WorkHave(o);
+        nextWorkActors = new ArrayList<WorkActor>();
+    }
+    public WorkActor next(String s) throws ClassNotFoundException, IllegalAccessException, InstantiationException
+    {
+        nextWorkActors.add(new WorkActor(ChangeStringToWork(s)));
+        return this;
+    }
+    private work ChangeStringToWork(String s)  throws ClassNotFoundException, IllegalAccessException, InstantiationException
+    {
+        Object k = Class.forName(s).newInstance();
+        if(!(k instanceof work)) throw new ClassNotFoundException(); //todo may define new Exception
+        return (work)k;
     }
     private WorkActor giveRes(WorkHave res)
     {
@@ -30,7 +53,7 @@ public class WorkActor {
     }
     public void working() throws Exception {
         WorkHave newRes = new WorkHave(w.whatWillDo(res));
-        for(int i = 0; i< nextWorkActor.size(); i++)
+        for(int i = 0; i< nextWorkActors.size(); i++)
         {
             Controller.workdeals.enqueue(giveRes(newRes));
         }
