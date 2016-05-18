a713b85a1a131668c4c53880310c76b0a1d10470
JangWooJin <ehdwlsdnwls@naver.com>
2016-05-19 01:51:06 +0900

    Annotate Split file pane controller

--- src/Controller/SplitFilePaneController.java
+++ src/Controller/SplitFilePaneController.java
@@ -17,18 +17,30 @@ import java.io.File;
  * has Load, Save, Edit button action.
  * in Load button action, make file chooser.
  */
-public class SpliteFilePaneController {
+public class SplitFilePaneController {
     @FXML
     private TextArea left_text_area;
     @FXML
     private TextArea right_text_area;
+    /*
+    * 기본은 save 버튼 비활성화
+    * edit 버튼을 눌러서 수정이 가해진 상황 일 경우 save 버튼이 활성화 된다.
+    * 수정 사항을 저장한 후에는 다시 비활성화 된다.
+    * */
     @FXML
     private void clickLeftLoadButton(){
         fileChooser();
         System.out.println("Click");
     }
+    @FXML
+    private void clickRightLoadButton(){
+        fileChooser();
+        System.out.println("Click");
+    }
     /*
-    * 클릭하면 edit 이 가능하게 바꿈
+    * 기본은 text area 수정 불가
+    * edit 버튼을 클릭하면 수정 가능하게 바꿈
+    * 수정 가능한 상황에서 한 번 더 버튼을 누르면 다시 수정 불가.
     * */
     @FXML
     private void clickLeftEditButton() {
@@ -42,15 +54,6 @@ public class SpliteFilePaneController {
         }
     }
     @FXML
-    private void clickLeftSaveButton(){
-        System.out.println("Click");
-    }
-    @FXML
-    private void clickRightLoadButton(){
-        fileChooser();
-        System.out.println("Click");
-    }
-    @FXML
     private void clickRightEditButton() {
         boolean edit_flag = right_text_area.isEditable();
         if(edit_flag){
@@ -60,6 +63,10 @@ public class SpliteFilePaneController {
         else{
             right_text_area.setEditable(true);
         }
+    }
+    @FXML
+    private void clickLeftSaveButton(){
+        System.out.println("Click");
     }
     @FXML
     private void clickRightSaveButton(){
