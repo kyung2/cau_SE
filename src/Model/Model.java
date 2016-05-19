927d9e6952485419fb558e8a3852677ecc288de9
Jooingong <bcw6622@naver.com>
2016-05-19 14:56:12 +0900

    change models

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -9,13 +9,13 @@ import java.util.ArrayList;
 public interface Model {
     void newModel(int tapNum);
     int newModel();
-    void closeModel(int tapNum);
-    ArrayList<String> getText(int tapNum, int i);
-    void setText(int tapNum, ArrayList<String> text, int i);
-    void getTextOuter(int tapNum, String filepath, int i) throws IOException;
-    void setTextOuter(int tapNum, String filepath, int i) throws IOException;
-    void merge(int tapNum, int Index, boolean direction) throws IndexOutOfBoundsException;
-    ArrayList<ArrayList<String>[/*2*/]> getGroup(int tapNum);
-    ArrayList<String> getGroupLine(int tapNum, int i);
-    ArrayList<Integer> getGroupColor(int tapNum, int i);
+    void closeModel(int tapNum) throws IllegalAccessException;
+    ArrayList<String> getText(int tapNum, int i) throws IllegalAccessException;
+    void setText(int tapNum, ArrayList<String> text, int i) throws IllegalAccessException;
+    void getTextOuter(int tapNum, String filepath, int i) throws IOException, IllegalAccessException;
+    void setTextOuter(int tapNum, String filepath, int i) throws IOException, IllegalAccessException;
+    void merge(int tapNum, int Index, boolean direction) throws IndexOutOfBoundsException, IllegalAccessException;
+    ArrayList<ArrayList<String>>[] getGroup(int tapNum) throws IllegalAccessException;
+    ArrayList<String>[] getGroupLine(int tapNum, int i) throws IllegalAccessException;
+    ArrayList<Integer>[] getGroupColor(int tapNum, int i) throws IllegalAccessException;
 }
