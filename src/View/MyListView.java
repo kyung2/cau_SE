2f351841184977e18bbda9bb2d82d5037df1fbf8
JangWooJin <ehdwlsdnwls@naver.com>
2016-05-23 13:06:08 +0900

    Update My list view. restart to make

--- src/View/MyListView.java
+++ src/View/MyListView.java
@@ -15,41 +15,10 @@ import java.util.Iterator;
  * Created by woojin on 2016-05-21.
  */
 public class MyListView extends ListView<String> {
-    public ObservableList<String> list = FXCollections.observableArrayList();
-
     public MyListView(){
         super();
     }
 
-    public MyListView(String[] s){
-        super();
-        list = FXCollections.observableArrayList(s);
-        this.setItems(list);
-    }
-
-    public MyListView(ArrayList<ArrayList<String>> blocks){
-        super();
-        for(int i=0, n=blocks.size();i<n;i++) {
-            ArrayList<String> lines = blocks.get(i);
-            String one_block = new String();
-            for(int j = 0, m=lines.size(); j<m;j++){
-                one_block += lines.get(j);
-            }
-            list.add(i,one_block);
-        }
-        super.setItems(list);
-    }
-    public void setBlocks(ArrayList<ArrayList<String>> blocks){
-        for(int i=0, n=blocks.size();i<n;i++) {
-            ArrayList<String> lines = blocks.get(i);
-            String one_block = new String();
-            for(int j = 0, m=lines.size(); j<m;j++){
-                one_block += lines.get(j);
-            }
-            list.add(i,one_block);
-        }
-        super.setItems(list);
-    }
     public void setColorsOnBlock(int index){
 
     }
