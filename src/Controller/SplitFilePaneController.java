0de145548e0f65c3c81144412c09d4a3935e466c
kyung2 <lovelyhyunkyung@gmail.com>
2016-05-24 15:03:26 +0900

    merge

--- src/Controller/SplitFilePaneController.java
+++ src/Controller/SplitFilePaneController.java
@@ -9,6 +9,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.FileChooser;
 
@@ -38,6 +39,8 @@ public class SplitFilePaneController implements Initializable {
     private Button compare_button;
 
     private int tab_num;
+
+
     /*
     * 기본적으로
     * file pane 의 버튼은 로드 활성화. 수정 비활성화, 저장 비활성화
