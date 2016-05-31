517719a1b85022824e1ac7ca4e22871e2edbe3c1
kyung2 <lovelyhyunkyung@gmail.com>
2016-06-01 02:07:46 +0900

    Test2

--- src/View/HelpWindow2.java
+++ src/View/HelpWindow2.java
@@ -7,9 +7,9 @@ import java.io.IOException;
  * Created by hyunkyung on 2016-06-01.
  */
 
-/*이론상은 되어야하는데 안됨 추후 확인 */ 
+/*이론상은 되어야하는데 안됨 추후 확인 */
 public class HelpWindow2 {
-    public void HelpWindow() {
+    public void HelpWindow() throws IOException {
         Runtime run = Runtime.getRuntime();
         try {
             Process p = Runtime.getRuntime().exec("helper/simplemergehelper.chm");
