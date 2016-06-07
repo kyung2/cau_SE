790fd39c71a07750c7e5832b5427105e18da3228
JangWooJin <ehdwlsdnwls@naver.com>
2016-06-07 23:13:42 +0900

    Update Abstract file window class. add anotation

--- src/View/abstractFileWindow.java
+++ src/View/abstractFileWindow.java
@@ -14,8 +14,18 @@ import java.io.IOException;
 
 /**
  * Created by woojin on 2016-05-18.
+ * Open window 와 Save window 를 위한 Abstract class
+ * 원하는 title 과 controller file 을 받아서 stage 를 하나 만든다.
+ * 구성요소로는
+ * 생성자의 input 에 따라 결정되는 창의 Title,
+ * 상단에 이 창이 어떤 일을 하는지 알려주는 label
+ * 중간 상단에 'Left file' 이라고 적힌 label, 왼쪽 파일의 패스를 담을 text area, find 라 적힌 버튼
+ * 중간 하단에 'Right file' 이라고 적힌 label, 오른쪽 파일의 패스를 담을 text area, find 라 적힌 버튼
+ * 오른쪽 하단에 ok 버튼과 cancel 버튼
+ * 가장 아래쪽에 각종 상태를 알려주는 text area 로 구성된다.
  */
 abstract class AbstractFileWindow extends Stage {
+    /* title 과 controller class 를 받아 stage 를 구성해 주는 생성자*/
     protected AbstractFileWindow(String title, Object controllerFile) {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/Fxml/FileWindow.fxml"));
         loader.setController(controllerFile);
@@ -32,6 +42,10 @@ abstract class AbstractFileWindow extends Stage {
         this.getIcons().add(new Image("/View/Image/mainIcon.png"));
     }
 
+    /* 이 창이 어떤 일을 하는지 설명해 주는 부분을 반드시 만들어야 한다. */
+    abstract protected void initLabel();
+
+    /* 창을 구성하는 각종 구성요소들을 받아올 수 있는 method*/
     protected Label getWindowLabel(){
         return (Label)(((AnchorPane)this.getScene().getRoot()).getChildren().get(7));
     }
@@ -62,6 +76,4 @@ abstract class AbstractFileWindow extends Stage {
     protected Button getCancelButton(){
         return (Button)(((AnchorPane)this.getScene().getRoot()).getChildren().get(9));
     }
-
-    abstract protected void initLabel();
 }
